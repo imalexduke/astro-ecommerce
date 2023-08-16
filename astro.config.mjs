ecbb3ea599f7bde048fb0200a2524abbb3cffbaa
Alex Duke <98545370+imalexduke@users.noreply.github.com>
2023-08-17 02:12:28 +0500

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
-import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -8,7 +6,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
 });
