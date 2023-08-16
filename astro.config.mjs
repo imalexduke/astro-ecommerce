6d66dc255e2efd1bc522057572b038bc3a9aed6d
Alex Duke <98545370+imalexduke@users.noreply.github.com>
2023-08-17 02:10:53 +0500

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -6,15 +8,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce'
-});
-
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
-import netlify from '@astrojs/netlify/edge-functions';
-
-export default defineConfig({
+  export default defineConfig({
   output: 'server',
   adapter: netlify(),
 });
