3ad1a80134eaf805e5b839011adf56781c30955e
Alex Duke <98545370+imalexduke@users.noreply.github.com>
2023-08-17 02:00:52 +0500

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,3 @@ export default defineConfig({
   output: 'server',
   adapter: netlify(),
 });
-
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce'
-});
